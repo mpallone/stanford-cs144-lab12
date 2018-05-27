dd8cd07f3bde5038add3be9484c5ab0d5f5bccd9
cs144 <mark.c.pallone@gmail.com>
2018-05-26 18:55:12 -0700

    Add go_client.sh and go_server.sh

---
+++ go_client.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+make clean
+make
+sudo valgrind --leak-check=full --show-leak-kinds=all ./ctcp -p 9999 -c localhost:8888
+# ./ctcp -p 9999 -c localhost:8888
---
+++ go_server.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+set -e
+make clean
+make
+sudo valgrind --leak-check=full --show-leak-kinds=all ./ctcp -s -p 8888
+# sudo ./ctcp -s -p 8888
